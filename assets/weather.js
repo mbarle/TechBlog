8ec1388089c78aa616bb4cd8668b4939b11b0840
mbarle <marusa.barle@gmail.com>
2026-07-16 14:03:44 -0700

    Added the js for open meteo weather API.

---
+++ assets/weather.js
@@ -0,0 +1,61 @@
+const url =
+  "https://api.open-meteo.com/v1/forecast" +
+  "?latitude=46.0569" +
+  "&longitude=14.5058" +
+  "&current=temperature_2m,weather_code";
+
+async function loadWeather() {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    document.getElementById("weather_code").textContent =
+      `${weatherDescriptions[data.current.weather_code]} `;
+
+    document.getElementById("temperature").textContent =
+      `${data.current.temperature_2m} °C`;
+
+    console.log(data.current.temperature_2m);
+    console.log(data.current.weather_code);
+    console.log(data);
+    console.log(weatherDescriptions[data.current.weather_code]);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+loadWeather();
+
+const weatherDescriptions = {
+  0: "Jasno",
+  1: "Pretežno jasno",
+  2: "Delno oblačno",
+  3: "Oblačno",
+  45: "Megla",
+  48: "Megla z ivjem",
+  51: "Rahlo rosenje",
+  61: "Rahel dež",
+  63: "Dež",
+  65: "Močan dež",
+  71: "Rahlo sneženje",
+  80: "Plohe",
+  95: "Nevihta"
+};
+
+const weatherIcons = {
+  0: "☀️",
+  1: "🌤️",
+  2: "⛅",
+  3: "☁️",
+  61: "🌧️",
+  71: "❄️",
+  95: "⛈️"
+};
+
+
+
