91b07dd19d17906188d931c68d103082c855ae66
mbarle <marusa.barle@gmail.com>
2026-08-30 11:33:48 -0700

    Replaced the hard-coded location with a location from acquired geolocation data.

--- assets/js/geolocation.js
+++ assets/js/geolocation.js
@@ -1,5 +1,7 @@
 console.log("geolocation.js se je naložil");
 
+
+
 function getWeather() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(success, error);
@@ -18,6 +20,29 @@ function success(position) {
     console.log("Pridobljena dolžina:", longitude);
 
 	loadWeather(latitude, longitude);
+	getLocationName(latitude, longitude);
+}
+
+async function getLocationName(latitude, longitude) {
+    const response = await fetch(
+        `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`
+    );
+
+    const data = await response.json();
+
+    console.log("Reverse geocoding:", data);
+
+    const place =
+        data.address.city ||
+        data.address.town ||
+        data.address.village ||
+        data.address.municipality ||
+        "Unknown location";
+
+
+    const country = data.address.country;
+
+    document.getElementById("location").textContent = `${place}, ${country}`;
 }
 
 // call of function to acquire weather 
@@ -40,4 +65,6 @@ function error(error) {
 document.addEventListener("DOMContentLoaded", () => {
   getWeather();
 });
+
+
 
